4a170665a948beff3fc87e4c42705500a11ad361
AqsaNabi <miraqsa186@gmail.com>
2026-09-17 16:47:46 +0530

    Updated code

--- 17thSep/main.py
+++ 17thSep/main.py
@@ -1,6 +1,9 @@
-from pydantic import BaseModel, EmailStr, AnyUrl, Field, field_validator
+from pydantic import BaseModel, EmailStr, AnyUrl, Field, field_validator, model_validator, computed_field
 from typing import List, Dict, Optional, Annotated
-
+class AAddress(BaseModel):
+    city: str
+    state: str
+    pin: str
 
 class patient(BaseModel):
     #type validation
@@ -12,6 +15,16 @@ class patient(BaseModel):
     allergies: Annotated[Optional[list[str]], Field(default=None, max_length=5)]#to validate the data within the list
     contact_details: Dict[str, str]
     link:AnyUrl
+    height: float
+    address: AAddress # is a complex data model which will have its own other field, we can create a nested model 
+
+
+
+    @computed_field
+    @property 
+    def calculated_bmi(self)-> float:
+        bmi =round(self.height/(self.weight**2), 2)
+        return bmi
 
     @field_validator('email')
     @classmethod
@@ -27,7 +40,12 @@ class patient(BaseModel):
     @classmethod  
     def transform_name(cls,value):
         return value.upper()
-    
+    @model_validator(mode='after')
+    def validate_emergency_contacts(cls, model):
+        if model.age>60 and 'emergency' not in model.contact_details:
+            raise ValueError('Patients older than 60 must have an emerrgency contact')
+        return model
+         
 
 def inst_data(patient: patient):
     print(patient.name)
@@ -40,9 +58,8 @@ def inst_data(patient: patient):
     print(patient.email)
     print(patient.link)
     print("data installed")
-   
-
-
+    print('BMI', patient.calculated_bmi)
+    
     
 
 def update_data(patient: patient):
@@ -51,11 +68,20 @@ def update_data(patient: patient):
     print("data updated")
 
 
-patient_info={'name': 'aqsa', 'age':'70', 'weight': 88.9, 'married': True, 'contact_details': {'phone': 'XXXXXXXXX'}, 'email':'abc@hdfc.com', 'link':'https://example.com/bb/mk'}
+
+Add_dict={'city': 'srinagar', 'state':'kashmir', 'pin':'190003'}
+
+add1= AAddress(**Add_dict)
+
+
+
+patient_info={'name': 'aqsa', 'age':'70','height':183 ,'weight': 88.9, 'married': True, 'contact_details': {'phone': 'XXXXXXXXX', 'emergency':'xxxxxxxx'}, 'email':'abc@hdfc.com', 'link':'https://example.com/bb/mk', 'address': add1}
 
 p1=patient(**patient_info)
-
-
 inst_data(p1)
+print(p1)
+print(p1.address.pin)
+temp =p1.model_dump(include=['name'])    
 
-        
+print(temp)
+print(type(temp))
